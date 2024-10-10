f7837c7aa14998fed2e9789ddbe88babd54bca11
sjaspal01 <jaspal0113@gmail.com>
2024-10-11 01:35:07 +0530

    Updated

--- online_trip_management/src/main/java/com/masai/OnlineTripManagementApplication.java
+++ online_trip_management/src/main/java/com/masai/OnlineTripManagementApplication.java
@@ -1,13 +1,1 @@
-package com.masai;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class OnlineTripManagementApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineTripManagementApplication.class, args);
-	}
-
-}
